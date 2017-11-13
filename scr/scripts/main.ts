e35c528bf4681eaf75317d1e5beca430750beba4
Björn Kämmereit <bjoern.kaemmereit@project-collins.com>
2017-11-13 23:08:57 +0100

    Views separated in Home and Experience

--- scr/scripts/main.ts
+++ scr/scripts/main.ts
@@ -2,44 +2,46 @@ import Dashboard from "./dashboard";
 
 declare let COBI: any;
 
-let dashboard = new Dashboard('container', window.innerWidth, window.innerHeight);
-
-dashboard.initView();
-
-dashboard.initAverageSpeedView();
-dashboard.initAverageCadenceView();
-
-dashboard.initSpeedCheck();
-dashboard.initCadenceCheck();
+let dashboard = new Dashboard('experience', window.innerWidth, window.innerHeight);
 
 COBI.init('token — can by anything right now');
 
-COBI.app.theme.subscribe(
-    function (value) {
-        console.log(value)
-    }
-);
+if (COBI.parameters.state() === 'experience') {
 
-COBI.rideService.speed.subscribe(
-    function (value) {
-        dashboard.updateCurrentSpeedView(value * 3.6);
-    }
-);
+    dashboard.initExperienceView();
 
-COBI.tourService.averageSpeed.subscribe(
-    function (value) {
-        dashboard.updateAverageSpeedView(value * 3.6);
-    }
-);
+    COBI.app.theme.subscribe(
+        function (value) {
+            console.log(value)
+        }
+    );
 
-COBI.rideService.cadenceAvailability.subscribe(
-    function (value) {
-        console.log('cadence available: ' + value);
-    }
-);
+    COBI.rideService.speed.subscribe(
+        function (value) {
+            dashboard.updateCurrentSpeedView(value * 3.6);
+        }
+    );
 
-COBI.rideService.cadence.subscribe(
-    function (value) {
-        dashboard.updateCurrentCadenceView(value);
-    }
-);
+    COBI.tourService.averageSpeed.subscribe(
+        function (value) {
+            dashboard.updateAverageSpeedView(value * 3.6);
+        }
+    );
+
+    COBI.rideService.cadenceAvailability.subscribe(
+        function (value) {
+            console.log('cadence available: ' + value);
+        }
+    );
+
+    COBI.rideService.cadence.subscribe(
+        function (value) {
+            dashboard.updateCurrentCadenceView(value);
+        }
+    );
+}
+else {
+    dashboard.initHomeView();
+}
+
+document.getElementById('version').innerHTML = Dashboard.getVersion();
